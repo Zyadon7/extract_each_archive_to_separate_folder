cc3149d0bd4c79deb8fafe70c8e137145d39f4be
Zyadon7 <Zyadon7@gmail.com>
2023-07-13 16:27:04 -0400

    no module named rarfile

--- extract_each_archive_to_separate_folder.py
+++ extract_each_archive_to_separate_folder.py
@@ -2,7 +2,9 @@ import os
 import shutil
 import tkinter as tk
 from tkinter import filedialog
-import patoolib
+import zipfile
+import rarfile
+import py7zr
 
 # Open file dialog to select directory
 root = tk.Tk()
@@ -11,11 +13,24 @@ directory = filedialog.askdirectory(title="Select Directory")
 
 # Unzip all archives in the selected directory
 for filename in os.listdir(directory):
-    if filename.endswith(".zip") or filename.endswith(".rar") or filename.endswith(".7z"):
+    if filename.endswith(".zip"):
         archive_path = os.path.join(directory, filename)
         output_folder = os.path.splitext(archive_path)[0]
         os.makedirs(output_folder, exist_ok=True)
-        patoolib.extract_archive(archive_path, outdir=output_folder)
+        with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+            zip_ref.extractall(output_folder)
+    elif filename.endswith(".rar"):
+        archive_path = os.path.join(directory, filename)
+        output_folder = os.path.splitext(archive_path)[0]
+        os.makedirs(output_folder, exist_ok=True)
+        with rarfile.RarFile(archive_path, 'r') as rar_ref:
+            rar_ref.extractall(output_folder)
+    elif filename.endswith(".7z"):
+        archive_path = os.path.join(directory, filename)
+        output_folder = os.path.splitext(archive_path)[0]
+        os.makedirs(output_folder, exist_ok=True)
+        with py7zr.SevenZipFile(archive_path, mode='r') as z7_ref:
+            z7_ref.extractall(output_folder)
 
 # Open the directory in File Explorer
 if os.name == "nt":  # Windows OS
