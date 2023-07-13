da4a29f01144b01a78039c690db781c7504fb4c2
Zyadon7 <Zyadon7@gmail.com>
2023-07-13 16:59:35 -0400

    switching to bard.ai

--- extract_each_archive_to_separate_folder.py
+++ extract_each_archive_to_separate_folder.py
@@ -4,35 +4,35 @@ import tkinter as tk
 from tkinter import filedialog
 import zipfile
 import lzma
+import subprocess
 
-# Open file dialog to select directory
-root = tk.Tk()
-root.withdraw()
-directory = filedialog.askdirectory(title="Select Directory")
+def unzip_archives(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".zip"):
+            archive_path = os.path.join(directory, filename)
+            output_folder = os.path.splitext(archive_path)[0]
+            os.makedirs(output_folder, exist_ok=True)
+            with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+                zip_ref.extractall(output_folder)
+        elif filename.endswith(".7z"):
+            archive_path = os.path.join(directory, filename)
+            output_folder = os.path.splitext(archive_path)[0]
+            os.makedirs(output_folder, exist_ok=True)
+            with open(archive_path, 'rb') as file_ref:
+                file_content = file_ref.read()
+                with lzma.open(output_folder, 'wb') as output_file:
+                    output_file.write(file_content)
+        elif filename.endswith(".rar"):
+            archive_path = os.path.join(directory, filename)
+            output_folder = os.path.splitext(archive_path)[0]
+            os.makedirs(output_folder, exist_ok=True)
+            subprocess.run(['unrar', 'x', archive_path, output_folder])
 
-# Unzip all archives in the selected directory
-for filename in os.listdir(directory):
-    if filename.endswith(".zip"):
-        archive_path = os.path.join(directory, filename)
-        output_folder = os.path.splitext(archive_path)[0]
-        os.makedirs(output_folder, exist_ok=True)
-        with zipfile.ZipFile(archive_path, 'r') as zip_ref:
-            zip_ref.extractall(output_folder)
-    elif filename.endswith(".rar"):
-        archive_path = os.path.join(directory, filename)
-        output_folder = os.path.splitext(archive_path)[0]
-        os.makedirs(output_folder, exist_ok=True)
-        shutil.unpack_archive(archive_path, output_folder, "rar")
-    elif filename.endswith(".7z"):
-        archive_path = os.path.join(directory, filename)
-        output_file = os.path.splitext(archive_path)[0] + '.txt'
-        with open(archive_path, 'rb') as file_ref:
-            file_content = file_ref.read()
-            with lzma.open(output_file, 'wb') as output_file:
-                output_file.write(file_content)
+def main():
+    root = tk.Tk()
+    root.withdraw()
+    directory = filedialog.askdirectory(title="Select Directory")
+    unzip_archives(directory)
 
-# Open the directory in File Explorer
-if os.name == "nt":  # Windows OS
-    os.startfile(directory)
-elif os.name == "posix":  # Linux/Unix OS
-    os.system(f"xdg-open {directory}")
+if __name__ == "__main__":
+    main()
